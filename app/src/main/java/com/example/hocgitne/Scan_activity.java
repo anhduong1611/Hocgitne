39672cb0de6d1a92678eedb93d9ba457555f4db3
Ho anh duong <hothianhduong16112002@hmail.com>
2022-07-11 14:32:34 +0700

    hihi

---
+++ app/src/main/java/com/example/hocgitne/Scan_activity.java
@@ -0,0 +1,15 @@
+package com.example.hocgitne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Scan_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        //hihi
+    }
+}
---
+++ app/src/main/java/com/example/hocgitne/koko.java
@@ -0,0 +1,4 @@
+package com.example.hocgitne;
+
+public class koko {
+}
