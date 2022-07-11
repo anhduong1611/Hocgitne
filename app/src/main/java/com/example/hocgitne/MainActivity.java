1fdc97c501397f25c9bd4edfdf5e87209dadd3c9
Ho anh duong <hothianhduong16112002@hmail.com>
2022-07-11 14:54:02 +0700

    koko

--- app/src/main/java/com/example/hocgitne/MainActivity.java
+++ app/src/main/java/com/example/hocgitne/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Scan_activity scan_activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/example/hocgitne/koko.java
+++ app/src/main/java/com/example/hocgitne/koko.java
@@ -1,4 +1,5 @@
 package com.example.hocgitne;
 
 public class koko {
+    //khihihi
 }
